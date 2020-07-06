51301fc2a28a9b266d9d9c97304f40f666adeb74
Oliver Inglis <ollie@theinglistwins.com>
2020-07-06 18:37:02 +0100

    current work

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,6 +1,7 @@
 const initCarosel = () => {
   if(document.querySelector('.top-image')){
     const carosel = document.querySelector('.top-image');
+    carosel.style.backgroundImage = `url(../img/space.jpg)`
   }
 }
 
